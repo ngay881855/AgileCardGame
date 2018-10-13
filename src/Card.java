cca08359482ea8e8a5e8e70000e75949c302c6f6
DESKTOP-T76K5E9\Thien An <ngay881855@gmail.com>
2018-10-13 03:34:28 -0500

    Add Card class
    Note: Implemented based on SprintZero-Slide 3

---
+++ src/Card.java
@@ -0,0 +1,32 @@
+
+public class Card {
+	// Question for the card
+	private String sQuestion;
+	// Answer for the card
+	private String sAnswer;
+	
+	public String getsQuestion() {
+		return sQuestion;
+	}
+	public void setsQuestion(String sQuestion) {
+		this.sQuestion = sQuestion;
+	}
+	public String getsAnswer() {
+		return sAnswer;
+	}
+	public void setsAnswer(String sAnswer) {
+		this.sAnswer = sAnswer;
+	}
+	
+	// The idea is to compare the question card with the user selected card,
+	// if both the question and answer are match, then the answer is correct
+	// Return value: True == Correct || False == Incorrect
+	public boolean checkResult(Card answerCard) {
+		boolean result = false;
+		
+		if (this.sQuestion == answerCard.sQuestion && this.sAnswer == answerCard.sAnswer) {
+			result = true;
+		}
+		return result;
+	}
+}
